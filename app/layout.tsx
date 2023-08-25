260acf261294d501cf21a55a131288a315f8679b
Mohammad <m.agame1379@gmail.com>
2023-08-25 22:17:48 +0330

    type error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Car Showcase",
+  title: "Car Hub",
   description: "The best cars in the world",
 };
 
